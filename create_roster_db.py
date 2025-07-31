df1a3ed9a67b73ef58bb69b8de7b040c730b45b2
SeanPiedmonte <seanpiedmonte@gmail.com>
2025-07-30 21:23:38 -0500

    [#2] Ironwood Roster into the db

--- create_roster_db.py
+++ create_roster_db.py
@@ -2,31 +2,41 @@
 # in development
 
 import sqlite3
+import csv
+
+player_id = -1
+ironwood_roster_file = open("ironwood.csv", "r")
+ironwoodcsv = csv.reader(ironwood_roster_file)
 
 db = sqlite3.connect("rosters.db")
 cur = db.cursor()
-
 # create the players table if it does not exist
 create_players_stmt = """CREATE TABLE IF NOT EXISTS players(playerid INTEGER PRIMARY KEY,
                                     name TEXT,
+                                    number INTEGER,
                                     position TEXT,
-                                    hometown TEXT,
-                                    homestate TEXT,
                                     star_rating INTEGER,
                                     year INTEGER,
                                     redshirt INTEGER,
-                                    dealbreaker TEXT,
-                                    number INTEGER,
                                     height INTEGER,
                                     weight INTEGER,
                                     speed INTEGER,
-                                    throwing_ability INTEGER,
-                                    strength INTEGER,
+                                    throwing_power INTEGER,
+                                    throwing_accuracy INTEGER,
                                     catching INTEGER,
                                     blocking INTEGER,
+                                    pass_rush INTEGER,
+                                    block_shedding INTEGER,
                                     tackling INTEGER,
                                     coverage INTEGER,
-                                    pass_rush INTEGER)"""
+                                    strength INTEGER,
+                                    football_iq INTEGER,
+                                    ball_carrier_vision INTEGER,
+                                    carrying INTEGER,
+                                    dev_trait INTEGER,
+                                    dealbreaker TEXT,
+                                    hometown TEXT,
+                                    homestate TEXT)"""
 cur.execute(create_players_stmt)
 
 create_teams_stmt = """CREATE TABLE IF NOT EXISTS teams(teamid INTEGER PRIMARY KEY,
@@ -62,13 +72,20 @@ db.commit()
 
 true = 1
 false = 0
-#playerid,name,position,hometown,homestate,star_rating,year,redshirt,dealbreaker,number,height,weight,speed,throwing_ability,strength,catching,blocking,tackling,coverage,pass_rush
-ironwood_state_roster = [
-    (0, "Carter Malone", "QB", "Scottsdale", "AZ", 4, 2, false, "scheme_fit", 12, 76, 225, 87, 81, 83, 17, 10, 10, 5, 5),
-    (1, "Julian Barnes", "QB", "Bowling Green", "KY", 3, 1, true, "playing_time", 14, 74, 210, 74, 69, 75, 13, 10, 6, 3, 3),
-    (2, "Elijah Cooke", "QB", "Chattanooga", "TN", 3, 3, false, "program_tradition", 8, 75, 215, 75, 84, 74, 20, 12, 10, 5, 5),
-    (3, "Jace Hollowell", "QB", "Sioux Falls", "SD", 2, 0, true, "distance_to_home", 16, 73, 195, 67, 49, 62, 10, 8, 3, 1, 2),
-]
+tup_list = []
+for line in ironwoodcsv:
+    if player_id == -1:
+        player_id+=1
+        continue
+    line.insert(0, str(player_id))
+    player_id+=1
+    tup_list.append(tuple(line))
 
-cur.executemany("INSERT INTO players VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", ironwood_state_roster)
+cur.executemany("""INSERT INTO players(playerid, name, number, position, 
+                       star_rating, year, redshirt, height, weight, speed,
+                       throwing_power, throwing_accuracy, catching, blocking, 
+                       pass_rush, block_shedding, tackling, coverage, strength, 
+                       football_iq, ball_carrier_vision, carrying, dev_trait, 
+                       dealbreaker, hometown, homestate) 
+                       VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)""", tup_list)
 db.commit()
