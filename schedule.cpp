b899d67386531fe34d206b886e7e3f90991ffd87
SeanPiedmonte <seanpiedmonte@gmail.com>
2025-11-04 05:59:59 -0600

    add a new scheduling algo and remove unnecessary file

--- schedule.cpp
+++ schedule.cpp
@@ -1,6 +1,3 @@
-#include <time.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "schedule.h"
 
 void create_schedule(schedule sch, int teams, int weeks) {
@@ -9,14 +6,11 @@ void create_schedule(schedule sch, int teams, int weeks) {
     int away_ptr = 9;
     int anchor_pair = 5;
     for (int i = 1; i < 10; i++) {
-        printf("anchor: %d, anchor_pair: %d, home_ptr: %d, away_ptr: %d\n", anchor, anchor_pair, home_ptr, away_ptr);
         sch[anchor][i-1] = anchor_pair;
         sch[anchor_pair][i-1] = anchor;
         int in_ptr_h = home_ptr;
         int in_ptr_a = away_ptr;
-        printf("%d:\n", i);
         for (int j = 0; j < teams/2-1; j++) {
-            printf("in_ptr_h: %d, in_ptr_a: %d\n", in_ptr_h, in_ptr_a);
             sch[in_ptr_h][i-1] = in_ptr_a;
             sch[in_ptr_a][i-1] = in_ptr_h;
             in_ptr_h++;
